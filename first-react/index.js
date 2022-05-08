61c70f5fbce809ab5a1e34b752dcc9995d531a23
Rafael Suárez <rafael.asg@gmail.com>
2022-05-08 11:49:13 -0400

    lab react

--- first-react/index.js
+++ first-react/index.js
@@ -1,9 +1,7 @@
-
 const navbar = (
         <div className="container">
             <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-            
-
+           
             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
                 <li><a href="#" className="nav-link px-2 link-dark">Features</a></li>
