55d0836ff3186436946a21cd69e9a07e8453c31c
Rafael Suárez <rafael.asg@gmail.com>
2022-05-08 22:52:28 -0400

    theme primary color

--- react-app/src/theme.js
+++ react-app/src/theme.js
@@ -5,7 +5,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#0074C4',
     },
     secondary: {
       main: '#19857b',
